de3314b75069ee587f7628f5ae8b796a5e9199b7
joanbs18 <110201780+joanbs18@users.noreply.github.com>
2025-05-11 11:00:52 -0600

    Update FlipCard.jsx

--- resources/js/components/FlipCard.jsx
+++ resources/js/components/FlipCard.jsx
@@ -2,17 +2,33 @@ import "../../css/flipCard.css";
 
 export default function FlipCard() {
     return (
-      
         <div className="flip-card">
             <div className="flip-card-inner">
                 <div className="flip-card-front">
-                    <img  src="images/Elberth Logo-blanco.webp" alt="" width={95} height={80}/>
-                    <h1 className="title">Elberth</h1>
-                    <p>Hover Me</p>
+                    <img
+                        src="images/Elberth Logo-blanco.webp"
+                        alt="Logo de Elberth"
+                        width={95}
+                        height={80}
+                    />
+                    <h1 className="title">Elberth Guzmán</h1>
+                    <p>Masajista · Fisioterapeuta</p>
                 </div>
                 <div className="flip-card-back">
-                    <p className="title">BACK</p>
-                    <p>Leave Me</p>
+                    <p className="title">Acerca de mí</p>
+                    <ul>
+                        <li>✔️ Licenciado en Educación Física</li>
+                        <li>✔️ Especializado en terapia física</li>
+                        <li>✔️ Masajista terapéutico</li>
+                        <li>✔️ Experiencia en recuperación muscular</li>
+                        <li>✔️ Técnicas de relajación</li>
+                        <li>✔️ Rehabilitación física personalizada</li>
+                    </ul>
+                    <p>
+                        📞 8849-4151
+                        <br />
+                        📧 elbertguzman@gmail.com
+                    </p>
                 </div>
             </div>
         </div>
